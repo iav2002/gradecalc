7907fdeb03718441e85af195eb3f7f0f3ec0bde0
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2026-02-03 23:57:38 +0000

    UserRepository Interface added

---
+++ src/main/java/com/ignacio/gradecalc/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ignacio.gradecalc.repository;
+
+import com.ignacio.gradecalc.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
