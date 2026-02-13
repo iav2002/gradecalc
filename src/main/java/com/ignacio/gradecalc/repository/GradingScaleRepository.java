5b1d6ab7c11b585dd0eae25926a24b182b704367
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2026-02-13 19:52:19 +0000

    Interfaces updated

--- src/main/java/com/ignacio/gradecalc/repository/GradingScaleRepository.java
+++ src/main/java/com/ignacio/gradecalc/repository/GradingScaleRepository.java
@@ -1,5 +1,4 @@
 package com.ignacio.gradecalc.repository;
-
 import com.ignacio.gradecalc.entity.GradingScale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
